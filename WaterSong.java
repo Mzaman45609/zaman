04e6fd576a225da3dbeb3000d1376e5aea7637c7
Mzaman45609 <47962236+Mzaman45609@users.noreply.github.com>
2019-04-24 18:50:04 +0500

    comit 8

--- WaterSong.java
+++ WaterSong.java
@@ -6,29 +6,20 @@ public static void main(String args[])
 
 int waterNum=9;
 String word="bottles";
-while (waterNum>0)
+while(waterNum>0)
 {
-if(waterNum==1)
+if(waterNum=1)
 {
-word= "bottles";
+word="bottle";
 }
 
 System.out.println(waterNum + " "+ word + "of water on the wall");
-
 System.out.println(waterNum + " "+ word + "of water");
-
-
 System.out.println("take one down");
-
-
-
-System.out.println("pass it around");
+System.out.println(pass it around");
 
 waterNum=waterNum-1;
-
-
-if(waterNum >0)
-
+if(waterNum>0)
 {
 System.out.println(waterNum + " "+ word + "of water on the wall");
 
